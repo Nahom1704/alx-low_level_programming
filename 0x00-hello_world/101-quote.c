b10cf03fbb978c3c6c0ae9b977b2f69675623dca
nahom mulatu <nahome.mulatu@yahoo.com>
2022-06-02 13:38:28 -0700

    add

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints the size of various types
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+
+int main()
+
+{	
+	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
+
+}
